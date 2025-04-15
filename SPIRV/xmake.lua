6f99c20534df2ccd1dfab842ce37fbcf97179282
Yuefeng Geng <maxwellgeng@outlook.com>
2025-04-15 09:20:46 +0800

    use object

--- SPIRV/xmake.lua
+++ SPIRV/xmake.lua
@@ -1,7 +1,7 @@
 target("spirv_builder")
 add_rules("lc_basic_settings", {
-    project_kind = "binary",
+    project_kind = "object",
 })
 add_includedirs(path.directory(os.scriptdir()), {public = true})
-add_files("SpvBuilder.cpp", "InReadableOrder.cpp", "disassemble.cpp", "doc.cpp", "test.cpp")
+add_files("SpvBuilder.cpp", "InReadableOrder.cpp", "disassemble.cpp", "doc.cpp")
 target_end("spirv_builder")
